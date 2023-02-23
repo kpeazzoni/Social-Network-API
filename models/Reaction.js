fdafa019abac9d6aac982be7ed500418da67a6b9
Karen Peazzoni <119969744+kpeazzoni@users.noreply.github.com>
2023-02-23 18:02:42 -0500

    added Reaction schema model

--- models/Reaction.js
+++ models/Reaction.js
@@ -3,6 +3,21 @@ const { Schema, Types } = require('mongoose');
 
 const reactionSchema = new Schema(
     {
-        
+    reactionId: {
+        type: Schema.Types.ObjectId,
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        maxLength: 280
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        lastAccessed: { type: Date, default: Date.now },
+    },
     }
-)
+);
